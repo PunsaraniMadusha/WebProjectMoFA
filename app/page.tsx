3a1007762fcd78e89a7f585f56e1473b0ecc8aed
PunsaraniMadusha <saranidharmasena2001@gmail.com>
2026-03-09 20:44:43 +0530

    welcome page eddied with add new image

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
       {/* Background Image */}
       <div className="absolute inset-0 z-0">
         <Image
-          src="/assets/new.jpeg" 
+          src="/assets/welcome.jpeg" 
           alt="Sri Lanka Landscape"
           fill
           className="object-cover"
